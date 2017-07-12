55e827b109e3b3f3607ef411d5f0532f3f66a9a9
drumnation <davidmieloch@gmail.com>
2017-07-12 11:56:09 -0400

    add project files

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # ReactVR Tour
 ## WebVR Menu and immersive 360 environments
 ![demo](demo.gif)
+[Watch Video](https://youtu.be/nPsGSknxuiw)
---
+++ index.vr.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react'
+import { AppRegistry, View, Text, Pano, asset } from 'react-vr'
+
+const places = [
+  {
+    title: 'Island Paradise',
+    image: 'island-garden.jpg'
+  }, 
+  {
+    title: 'Starry Night',
+    image: 'starry-sky.jpg'
+  },
+  {
+    title: 'Winter Outdoors',
+    image: 'winter-outdoor.jpg'
+  },
+  {
+    title: 'Light Show',
+    image: 'light-show.jpg'
+  }
+]
+
+class WorldTour extends Component {
+  constructor() {
+    super()
+    this.state = {
+      showMenu: false,
+      place: 'starry-sky.jpg'
+    }
+  }
+
+  toggleMenu() {
+    this.setState({showMenu: !this.state.showMenu})
+  }
+
+  render() {
+    return (
+      <View>
+        <Pano source={asset(this.state.place)}></Pano>
+        <View 
+          style={styles.menuButton}
+          onEnter={() => this.toggleMenu()}
+        >
+          <Text style={styles.menuButtonText}>
+            {this.state.showMenu ? 'Close Menu' : 'Open Menu'}
+          </Text>
+        </View>
+        {
+          this.state.showMenu ?
+            <View style={styles.menu}>
+              {
+                places.map((place, i) => {
+                  return (
+                    <View
+                      style={styles.menuItem} 
+                      key={i}
+                      onEnter={() => this.setState({place: place.image})}
+                    >
+                      <Text style={styles.menuItemText}>{place.title}</Text>  
+                    </View>
+                  )
+                })
+              }
+            </View>
+          :
+            <View></View>
+        }
+      </View>
+    )
+  }
+}
+
+const styles = {
+  menu: {
+    width: 5,
+    height: 1.25,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    transform: [
+      {translate: [-2, 0, -7.5]}
+    ]
+  },
+  menuButton: {
+    backgroundColor: '#fff',
+    borderRadius: 0.25,
+    width: 0.5,
+    height: 0.5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 0.01,
+    transform: [
+      {translate: [-2, 0, -5]}
+    ]
+  },
+  menuButtonText: {
+    textAlign: 'center',
+    fontSize: 0.15,
+    color: '#000'
+  },
+  menuItem: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 1,
+    height: 1,
+    borderRadius: 0.5,
+    borderWidth: 0.02,
+    backgroundColor: '#fff'
+  },
+  menuItemText: {
+    fontSize: 0.2,
+    textAlign: 'center',
+    color: '#000'
+  }
+}
+
+AppRegistry.registerComponent('WorldTour', () => WorldTour)
